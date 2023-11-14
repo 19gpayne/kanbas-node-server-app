ab6b6bc27085ca832ccebd81a35b493fadd9e2ee
Gwendolyn Payne <gwenpayne2012@yahoo.com>
2023-11-14 14:38:49 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+import express from "express";
+import cors from "cors"
+import CourseRoutes from "./courses/routes.js";
+import ModuleRoutes from "./modules/routes.js";
+import Hello from "./hello.js";
+import Lab5 from "./lab5.js";
+import "dotenv/config";
+import session from "express-session";
+const app = express();
+app.use(express.json());
+app.use(
+    cors({
+      credentials: true,
+      origin: process.env.FRONTEND_URL
+    })
+  );
+CourseRoutes(app);
+ModuleRoutes(app);
+Lab5(app);
+Hello(app);
+app.listen(process.env.PORT || 4000);
