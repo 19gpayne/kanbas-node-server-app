373a369ce7441ed9d4562f44042fa439f8f0c321
Gwendolyn Payne <gwenpayne2012@yahoo.com>
2023-11-26 19:06:19 -0500

    a6

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ import ModuleRoutes from "./modules/routes.js";
 import Hello from "./hello.js";
 import Lab5 from "./lab5.js";
 import "dotenv/config";
+import mongoose from "mongoose";
+import UserRoutes from "./users/routes.js";
+import session from "express-session";
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas"
+mongoose.connect(CONNECTION_STRING);
 const app = express();
 app.use(express.json());
 app.use(
@@ -13,8 +18,19 @@ app.use(
       origin: process.env.FRONTEND_URL
     })
   );
+const sessionOptions = {
+  secret: "any string",
+  resave: false,
+  saveUninitialized: false,
+};
+app.use(
+  session(sessionOptions)
+);
+  
+UserRoutes(app);
 CourseRoutes(app);
 ModuleRoutes(app);
 Lab5(app);
 Hello(app);
 app.listen(process.env.PORT || 4000);
+
